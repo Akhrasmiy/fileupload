eb53746c786b7c17b7577e83595d4bb3dab52c34
Jahongir <104265452+Akhrasmiy@users.noreply.github.com>
2023-06-08 16:59:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/project', {
   useUnifiedTopology: true,
 });
 app.use(express.urlencoded({ extended: true }))
-
 app.use(cors())
 app.use(userRout);
 app.use(fileRout);
