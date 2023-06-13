d33682b939512d927fec1c795bd66a17f91b50b3
Jahongir <akhrasmiy@gmail.com>
2023-06-13 06:38:49 +0500

    m

--- users.js
+++ users.js
@@ -164,9 +164,9 @@ router.post('/baycurs', IsLoggedIn, async (req, res, next) => {
   let teacher = await Teacher.findById(curs.teacher_Id)
  
   if (user.price >= curs.narxi) {
-    (teacher.hisob) += curs.narxi;
-    (user.price) -= curs.narxi*0.8;
-    (admin.hisobi)+=curs.narxi*0.2
+    (+teacher.hisob) += curs.narxi;
+    (+user.price) -= curs.narxi*0.8;
+    (+admin.hisobi)+=curs.narxi*0.2
     curs.subs.push(req.user.userId)
     user.mycurs.push({
       qachongacha: Math.floor(Date.now() / 1000 + curs.muddati * 30 * 24 * 60 * 60),
