b52987bb5af344ee28aa8cb533d8c1447cfb78fc
Jahongir <akhrasmiy@gmail.com>
2023-06-19 02:00:24 +0500

    m

--- teacher.js
+++ teacher.js
@@ -165,7 +165,6 @@ router.put('/teacher/', IsTeacherIn, async (req, res) => {
         mutahasislik,
         boglashlink
       }, { new: true });
-      teacher.save()
       res.send(teacher);
     } catch (error) {
       res.status(500).send(error);
