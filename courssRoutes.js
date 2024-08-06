280a1d92b075c4a25c77cffda1c80a025df0ca03
jaxmadov <jaxmadov@transoxiania.com>
2024-08-06 19:21:30 +0500

    changes

--- courssRoutes.js
+++ courssRoutes.js
@@ -422,21 +422,19 @@ router.get("/whoisownerbycard/:cardNumber", async (req, res, next) => {
   try {
     const cardNumber = req.params.cardNumber;
 
-    // Foydalanuvchi nomi va parolni Base64 formatida kodlash
-    const username = 'ilmlar';
+    const username = 'ilmlarcom';
     const password = 'dEpSPx^LWnK79VhC(EKh-A]*P';
     const authString = Buffer.from(`${username}:${password}`).toString('base64');
 
-    // So'rov yuborish
     const response = await axios.post('https://pay.myuzcard.uz/api/Credit/getCardOwnerInfoByPan', 
       { cardNumber: cardNumber },
       { headers: { Authorization: `Basic ${authString}` } });
 
-    console.log(response.data); // Ma'lumotlarni chop etish
-    res.send(response.data); // Faqatgina javob ma'lumotlarini yuborish
+    console.log(response.data);
+    res.send(response.data);
   } catch (error) {
     console.log(error);
-    res.status(400).json({ error: error.message }); // Faqatgina xato xabarini yuborish
+    res.status(400).json({ error: error });
   }
 });
 
