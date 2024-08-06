18edb4ef1285d243c327a2f4fee220ad01041816
jaxmadov <jaxmadov@transoxiania.com>
2024-08-06 18:32:57 +0500

    changes

--- courssRoutes.js
+++ courssRoutes.js
@@ -421,6 +421,7 @@ router.get("/courses-finish/:id",IsTeacherIn,async(req,res,next)=>{
 router.get("/whoisownerbycard/:cardNumber",async(req,res,next)=>{
   const id = req.params.cardNumber;
   const response = await axios.post('http://save.ilmlar.com/file', {cardNumber:id});
+  console.log(response)
   res.send(response)
 })
 
